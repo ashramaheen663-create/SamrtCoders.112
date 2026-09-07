70996e0108048bfef201bba8d5705e6a70a2b8cf
ashramaheen663-create <ashramaheen663@gmail.com>
2026-09-07 15:24:36 +0530

    Implement simple interest calculation in Prgm.py

---
+++ Prgm.py
@@ -0,0 +1,7 @@
+#ARITHMETIC OPERATORS 
+#CALCULATING SIMPLE INTEREST
+P=float(input(Enter Principle Amount:))
+R=float (input("Enter Rate of interest per yr"))
+T=float (input("Enter time in yrs"))
+SI=(P*R*T)/100
+print("Simple Interest=",SI)
