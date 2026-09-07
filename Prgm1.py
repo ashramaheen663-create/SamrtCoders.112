1c0f3879058daa5c60244fe8a013cd77d103535a
ashramaheen663-create <ashramaheen663@gmail.com>
2026-09-07 15:01:27 +0530

    Add arithmetic operations in Prgm1.py

---
+++ Prgm1.py
@@ -0,0 +1,9 @@
+#ARITHMETIC OPERATORS 
+A=int(input ("Enter First number:"))
+B=int(input ("Enter Second number:"))
+print(A,"+",B,"=",A+B)
+print(A,"-",B,"=",A-B)
+print(A,"*",B,"=",A*B)
+print(A,"/",B,"=",A/B)
+print(A,"%",B,"=",A%B)
+print(A,"//",B,"=",A//B)
