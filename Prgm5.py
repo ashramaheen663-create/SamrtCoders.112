7f382fc001d4fdf82761078ff9176ac3fcc9c5c4
ashramaheen663-create <ashramaheen663@gmail.com>
2026-09-07 23:05:45 +0530

    Implement divisibility checks for 3 and 5

---
+++ Prgm5.py
@@ -0,0 +1,9 @@
+N=int(input("Enter a Number:"))
+if N%3==0:
+  print("The number is divisible by 3")
+elif N%5==0:
+  print("The number is divisible by5")
+elif N%3==0 & n%5==0:
+  print("The number is divisible by 3 & 5")
+else:
+  print("The number is not divisible by both")
