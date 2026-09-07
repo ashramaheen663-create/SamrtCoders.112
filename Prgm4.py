d8d9c42f78b8f3a2037e46d5dc4b99476f6c3ebd
ashramaheen663-create <ashramaheen663@gmail.com>
2026-09-07 15:35:56 +0530

    Implement temperature conversion between Celsius and Fahrenheit
    This program allows users to convert temperatures between Celsius and Fahrenheit. It prompts for input in both scales and displays the corresponding temperature.

---
+++ Prgm4.py
@@ -0,0 +1,8 @@
+#ARITHMETIC OPERATORS 
+#CONVERTING CELCIUS TO FAHRENHEIT AND VICE VERSA 
+C=float(input("Enter Temperature in Celsius:"))
+F=(c*9/5)+32
+print("Temperature in Fahrenheit is",F)
+f=float(input("Enter Temperature in Fahrenheit:"))
+c=(f*5/9)-32
+print("Temperature in Celsius is",c)
