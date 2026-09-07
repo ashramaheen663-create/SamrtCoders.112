48565684a3a2a38873e96026817b4cc5c81b0c26
ashramaheen663-create <ashramaheen663@gmail.com>
2026-09-07 23:26:36 +0530

    Add program to compare three numbers for equality

---
+++ Prgm8.py
@@ -0,0 +1,8 @@
+#RELATIONAL AND LOGICAL OPERATORS
+A=int(input("Enter first number"))
+B=int(input("Enter second number"))
+C=int(input("Enter third number"))
+if A==B & B==C & C==A:
+  print("All the three numbers are Equal")
+else:
+  print("They are not Equal")
