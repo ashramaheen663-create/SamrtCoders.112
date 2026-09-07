ca4a8350e16dee1d42fb16fd0ff61d249deedac8
ashramaheen663-create <ashramaheen663@gmail.com>
2026-09-07 23:39:11 +0530

    Implement character type detection in Prgm10.py

---
+++ Prgm10.py
@@ -0,0 +1,10 @@
+#RELATIONAL AND LOGICAL OPERATORS
+ch=input("Enter a Character")
+if ch.isupper():
+  print("Uppercase")
+elif ch.islower():
+  print("Lowercase")
+elif ch.isdigit():
+  print("Digit")
+else:
+  print("Special character")
