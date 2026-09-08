e2ed3f03b0cfb9be709091892847616d8230b6dd
Ashra <ashramaheen@gmail.com>
2026-09-08 14:15:33 +0530

    Add program to categorize age groups in Prgm22.py

---
+++ Prgm22.py
@@ -0,0 +1,9 @@
+a=int(input("Enter Age:"))
+if a<=12:
+    print("You are a child.")
+elif a<=19:
+    print("You are a teenager.")
+elif a<=59:
+    print("You are an adult.")
+else:
+    print("You are a senior citizen.")
