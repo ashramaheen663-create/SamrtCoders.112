e78422a7eac6731b2d80b1ded10b690d3782b16e
Ashra <ashramaheen@gmail.com>
2026-09-08 13:15:13 +0530

    Add program to find the largest number among two numbers in Prgm18.py

---
+++ Prgm18.py
@@ -0,0 +1,8 @@
+#CONDITIONAL STATEMENTS
+#LARGEST NUMBER AMONG TWO NUMBERS
+A=int(input("Enter first number:"))
+B=int(input("Enter second number:"))
+if A>=B:
+    print(A,"is the largest number")
+else:
+    print(B,"is the largest number")
