ae9822b59a95bc1487f258e6472b55436e5d1437
Ashra <ashramaheen@gmail.com>
2026-09-09 12:01:33 +0530

    Add program to reverse a number in Prgm50.py

---
+++ Prgm50.py
@@ -0,0 +1,9 @@
+#LOOPS:DIGIT BASED PROBLEMS
+#REVERSE A NUMBER
+n = int(input("Enter a number: "))
+reverse = 0
+while n > 0:
+    digit = n % 10
+    reverse = reverse * 10 + digit
+    n = n // 10
+print("The reverse of the number is:", reverse)
